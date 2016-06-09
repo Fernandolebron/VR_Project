7de8d4511e7dd9c0430ec87e4edd98bfff6ecc3f
Fernandolebron <fernandolebron616@gmail.com>
2016-06-09 19:32:22 -0400

    Ultimos Cambios

--- app.js
+++ app.js
@@ -2,16 +2,25 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var world = require('./js/server_world');
+var world = require('./public/js/server_world');
 
+app.use(express.static(__dirname + '/public'));
+
+/*
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/WebVR.html');
+});*/
+
 // Add code to adjust public/static folders
 //
 //app.use( express.static(__dirname + '/js'));
-app.use(express.static(__dirname + '/js'));
+
+
+
 /*
 app.get('/js/client_world.js', function(req, res){
     res.sendFile(__dirname + '/js/client_world.js');
